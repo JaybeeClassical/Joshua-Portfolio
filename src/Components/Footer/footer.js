a31aa24afda1977a9bca608dc58e184fd44eb8f2
jaybeeClassical <johnboscoobiejesi@gmail.com>
2020-09-08 14:00:58 +0100

    added footer

---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Styles from '../Footer/footer.module.scss'
+
+const Footer = () => {
+    return (
+        <div className={Styles.footer__wrapper}>
+            <div className={Styles.footer__contact}>
+                <div className={Styles.footer__sayHello}>
+                    <h3>SAY HELLO</h3>
+                    <ul>
+                        <li>Mail Me here</li>
+                        <li>Chat me on telegram</li>
+                    </ul>
+                </div>
+
+                <div className={Styles.footer__shortcut}>
+                    <ul>
+                        <li>My Work</li>
+                        <li>My Shelf</li>
+                        <li>My Résumé</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={Styles.footer__horizontalLine}/>
+
+            <div className={Styles.footer__copyright}>
+                <p> Copyright Joshua Osagie </p>
+                <ul>
+                    <li>Twitter</li>
+                    <li>Linkedin</li>
+                    <li>Github</li>
+                    <li>Stack OverFlow</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
--- src/Components/layout.js
+++ src/Components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header/header.js';
 import Homepage from '../pages/Homepage/homepage'
+import Footer from '../Components/Footer/footer'
 
 
 const layout = () => {
@@ -8,6 +9,7 @@ const layout = () => {
         <div>
             <Header/>
             <Homepage/>
+            <Footer/>
         </div>
     )
 }
